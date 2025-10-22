463c0d1c144b6cd9cd1793f5e7fd40d69460301a
Wallace Ferreira <wallaceferreira149@gmail.com>
2025-10-21 21:04:57 -0300

    build: :tada: initial commit

---
+++ src/main/java/dev/arcanus/codestore/CodestoreApplication.java
@@ -0,0 +1,13 @@
+package dev.arcanus.codestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodestoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/arcanus/codestore/CodestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.arcanus.codestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
